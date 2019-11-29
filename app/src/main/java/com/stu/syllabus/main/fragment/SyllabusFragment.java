f77a2d32bc57cdca0be7bdc2932b5238ed7eaeea
ysf <yuansssf@gmail.com>
2019-11-29 13:32:28 +0800

    after first merge branch

--- app/src/main/java/com/stu/syllabus/main/fragment/SyllabusFragment.java
+++ app/src/main/java/com/stu/syllabus/main/fragment/SyllabusFragment.java
@@ -60,11 +60,9 @@ public class SyllabusFragment extends BaseFragment implements SyllabusContract.v
                 .syllabusModule(new SyllabusModule(this))
                 .build()
                 .inject(this);
-<<<<<<< HEAD
+
         syllabusPresenter.init();
-=======
-        pullToRefreshView.setRefreshing(true);
->>>>>>> dev_wwshe
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
@@ -87,13 +85,9 @@ public class SyllabusFragment extends BaseFragment implements SyllabusContract.v
 
     @Override
     public void init() {
-<<<<<<< HEAD
         syllabusRefreshLayout.setRefreshing(true);
         syllabusRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-=======
-        syllabusPresenter.init();
-        pullToRefreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
->>>>>>> dev_wwshe
+
             @Override
             public void onRefresh() {
                 syllabusPresenter.init();
